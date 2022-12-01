f9e08b12462323d4638e72ece233284786c50a74
Ali-raza <you@example.com>
2022-12-01 12:51:14 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/mediaquery.scss";
+import Footer from "./components/Footer";
 
 import Home from "./components/Home";
 
@@ -13,7 +19,10 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Home = () => {
   return (
     <>
-      <div className="home">
+      <div className="home" id="home">
         <main>
           <h1>TechStar</h1>
           <p>Solution to all your problems</p>
@@ -19,6 +25,47 @@ const Home = () => {
             explicabo officia esse, perferendis rerum vero error exercitationem
             atque eum ipsa tempore doloremque cum!
           </p>
+        </div>
+      </div>
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rem
+            praesentium fuga corrupti vero natus enim voluptas beatae, cumque
+            mollitia dicta veritatis perspiciatis cum totam suscipit quis
+            incidunt modi iure? ipsum dolor sit amet consectetur adipisicing
+            elit. Quaerat rem praesentium fuga corrupti vero natus enim voluptas
+            beatae, cumque mollitia dicta veritatis perspiciatis cum totam
+            suscipit quis incidunt modi iure praesentium fuga corrupti vero
+            natus enim voluptas beatae, cumque mollitia dicta veritatis
+            perspiciatis cum totam suscipit quis incidunt modi iure? ipsum dolor
+            sit amet consectetur adipisicing elit. Quaerat rem praesentium
+          </p>
+        </div>
+      </div>
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div style={{ animationDelay: "0.3s" }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div style={{ animationDelay: "0.5s" }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div style={{ animationDelay: "0.7s" }}>
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div style={{ animationDelay: "1s" }}>
+              <AiFillInstagram />
+              <p>Youtube</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
